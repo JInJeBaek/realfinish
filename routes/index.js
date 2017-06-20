db2be339f97f125e93b47338304e987cbcce98fc
finish <bjj4016@gmai.com>
2017-06-20 19:58:05 +0900

    zxx

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/keyboard', function(req, res)  {
+    const menu = {
+        type: 'button',
+        buttons: ['버튼1', '버튼2', '버튼3']
+    };
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(menu));
+});
+
+router.post('/message', function(req, res){
+    const _obj = {
+        user_key: req.body.user_key,
+        type: req.body.type,
+        content: req.body.content
+    };
+    console.log(_obj);
+
+    var message = {
+        "message": {
+            "text": "응답 메세지..."
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": [
+                "menu1",
+                "menu2",
+                "menu3"
+            ]
+        }
+    };
+});
+
+
 module.exports = router;
